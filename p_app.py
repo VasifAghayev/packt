fc5a01db4a605c180f36ec8d0e458f0e455f0e01
Vasif Aghayev <vasek333@yahoo.com>
2016-12-21 14:15:15 +0400

    Initial config

---
+++ p_app.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@add.route('/')
+def index():
+	return render_template("index.html")
+
+app.run(debug=True, port=8088, host='0.0.0.0')
